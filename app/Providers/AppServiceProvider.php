2da5f07e65da2dc3eca94a448003144a89b16b80
Juan Carlos Alvarez Lagos <juanklagos@outlook.com>
2025-11-29 03:28:14 -0500

    summary

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
               if ($this->app->environment('production')) {
-                  //  URL::forceRootUrl(config('app.url'));   // https://ruletaxpress.pro
+                    URL::forceRootUrl(config('app.url'));   // https://ruletaxpress.pro
                     URL::forceScheme('https');              // siempre https
                 }
     }
