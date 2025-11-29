c7f90ea38406300bdcac5384073c60ee0fd85fd4
Juan Carlos Alvarez Lagos <juanklagos@outlook.com>
2025-11-29 03:25:57 -0500

    summary

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
               if ($this->app->environment('production')) {
-                    URL::forceRootUrl(config('app.url'));   // https://ruletaxpress.pro
+                  //  URL::forceRootUrl(config('app.url'));   // https://ruletaxpress.pro
                     URL::forceScheme('https');              // siempre https
                 }
     }
